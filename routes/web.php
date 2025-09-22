e785812894c0a00f7a55026c5ee536e07d79d4a2
Yunus Kırca <ynskrca52@outlook.com>
2025-09-22 20:06:43 +0300

    added js css and images folders

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Ana sayfa
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+// Odalar
+Route::get('/rooms', function () {
+    return view('rooms.index');
+})->name('rooms.index');
+
+// Restoran
+Route::get('/restaurant', function () {
+    return view('restaurant');
+})->name('restaurant');
+
+// Hakkımızda
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+// Blog
+Route::get('/blog', function () {
+    return view('blog.index');
+})->name('blog.index');
+
+// İletişim
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
